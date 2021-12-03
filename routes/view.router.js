2928acf77d00ebbf52bab3a31b2fc6f272818201
Tobias Quante <t.quante@outlook.com>
2021-12-03 16:16:27 +0100

    feat: add about page

--- routes/view.router.js
+++ routes/view.router.js
@@ -1,6 +1,15 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const readPackageJsonFile = require('../util/filesystem.util');
 
-router.get('', (req, res) => res.render('home'))
-router.get('/about', (req, res) => res.render('about'))
+
+router.get('', (req, res) => res.render('home'));
+router.get('/about', async (req, res) => {
+  const packageJson = await readPackageJsonFile();
+  console.log(packageJson)
+  const aboutConfig = {
+    packageJson
+  }
+  res.render('about', aboutConfig)
+});
 
 module.exports = router;
---
+++ util/filesystem.util.js
@@ -0,0 +1,11 @@
+const fs = require("fs/promises")
+const path = require("path")
+
+async function readPackageJsonFile() {
+  const jsonBuffer = await fs.readFile(path.join(__dirname, '../package.json'));
+  const jsonString = Buffer.from(jsonBuffer).toString('utf-8')
+  return JSON.parse(jsonString);
+
+}
+
+module.exports = readPackageJsonFile
