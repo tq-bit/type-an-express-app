39593756f762b00140b1eae641c9a90a1f3efd5d
Tobias Quante <t.quante@outlook.com>
2021-12-04 15:29:27 +0100

    adjust layout

--- util/filesystem.util.js
+++ util/filesystem.util.js
@@ -5,7 +5,6 @@ async function readPackageJsonFile() {
   const jsonBuffer = await fs.readFile(path.join(__dirname, '../package.json'));
   const jsonString = Buffer.from(jsonBuffer).toString('utf-8')
   return JSON.parse(jsonString);
-
 }
 
 module.exports = readPackageJsonFile
